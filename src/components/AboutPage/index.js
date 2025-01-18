ec2b5f4a9bfe6411b040e1d197bb15034b037e6e
David Wakeham <david@rsh.dev>
2025-01-18 00:37:29 -0800

    Update index.js

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -148,7 +148,7 @@ const AboutPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
         >
 <h2 className={`text-3xl font-comfortaa font-bold ${textColor} mb-6`}><a href="">Shift reality.</a></h2>
 	    <p className={`${subtleText} text-lg mb-8`}>
-	    Inspired by places like Bell Labs, we believe discovery is not only the result of hard work, but the alchemy of creative play, wonder, and a sense of mission attached to urgent problems. We also believe in making our ideas accessible, crafting products that improve people's lives, and most importantly, keeping it fun.
+	    Inspired by places like Bell Labs, we believe discovery is not only the result of hard work, but a creative alchemy of play, wonder, and the sense of mission attached to urgent problems. We also believe in making our ideas accessible, and crafting products that improve people's lives.
           </p>
 <p className={`${subtleText} text-lg mb-8`}>
 Check out <a href="team" className="underline">careers</a> if this sounds like your jam!
