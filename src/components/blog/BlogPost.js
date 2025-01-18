74a1988bd1faef9edb50e8285e42e0ec0764a299
David Wakeham <david@rsh.dev>
2025-01-18 01:15:23 -0800

    Update BlogPost.js

--- src/components/blog/BlogPost.js
+++ src/components/blog/BlogPost.js
@@ -128,7 +128,7 @@ useEffect(() => {
             {/* Navigation */}
             <div className={`flex items-center justify-between pt-6 border-t ${borderColor}`}>
               <button 
-                onClick={() => navigate('/#blog')}
+                onClick={() => navigate('blog')}
                 className={`${textColor} hover:opacity-80 transition-opacity flex items-center gap-2`}
               >
           <div className={`text-sm font-comm`}><i>← Back to blog</i></div>
