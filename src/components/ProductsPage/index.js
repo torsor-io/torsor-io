c18ce86d8f857d1f309c9b5e11143ab0f956c0f0
David Wakeham <david@rsh.dev>
2025-01-17 16:43:08 -0800

    Update mailing list link

--- src/components/ProductsPage/index.js
+++ src/components/ProductsPage/index.js
@@ -49,8 +49,7 @@ const BlogPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
         >
           <div className={`text-lg text-center ${subtleText}`}>
 	  <a 
-  href="mailto:contact@rsh.dev"
-  className={`inline-block ${buttonBg} ${textColor} text-l px-6 py-3 rounded-lg transition-colors border ${borderColor}`}
+  href="mailto:updates@rsh.dev?subject=Subscribe&body=Please add me to the Redshift Labs mailing list!" className={`inline-block ${buttonBg} ${textColor} text-l px-6 py-3 rounded-lg transition-colors border ${borderColor}`}
 >
             <RocketLaunchIcon className={`h-6 w-6  ${textColor}`} />
           </a>
