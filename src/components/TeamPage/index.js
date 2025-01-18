172a31345ba60b60d17c21a29c5915db718b5634
David Wakeham <david@rsh.dev>
2025-01-18 00:46:03 -0800

    Add blurbs

--- src/components/TeamPage/index.js
+++ src/components/TeamPage/index.js
@@ -48,6 +48,7 @@ const BlogPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
           className="space-y-12"
         >
           <div className={`text-lg text-center ${subtleText}`}>
+	  <p>Team and careers information will be here soon!</p><br/>
 	  <a 
   href="mailto:updates@rsh.dev?subject=Subscribe&body=Please add me to the Redshift Labs mailing list!" className={`inline-block ${buttonBg} ${textColor} text-l px-6 py-3 rounded-lg transition-colors border ${borderColor}`}
 >
