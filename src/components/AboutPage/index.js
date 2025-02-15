2ba5058d9a455068fa420627d7a8fd7acd5e5a64
David Wakeham <david@rsh.dev>
2025-02-15 11:28:34 -0800

    Update index.js

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -150,9 +150,7 @@ const AboutPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
         >
 <h2 className={`text-3xl font-comfortaa font-bold ${textColor} mb-6`}><a href="/#puzzle">Shaped by difference.</a></h2>
 	    <p className={`${subtleText} text-lg mb-8`}>
-	    Inspired by places like Bell Labs, we believe discovery is not only the result of hard work, but a creative alchemy of play, wonder, and the sense of mission attached to urgent problems. We also believe in making our ideas accessible, and crafting tools that improve people's lives.
-          </p>
-<p className={`${subtleText} text-lg mb-8`}>
+	    Inspired by places like Bell Labs, we believe discovery is not only the result of hard work, but a creative alchemy of play, wonder, and the sense of mission attached to urgent problems.
 Check out <a href="#team" className="underline">careers</a> if this sounds like your jam!
 </p>
           <a 
