84399ab5a371f121b99707ccce581383187c8e38
David Wakeham <david@rsh.dev>
2025-04-18 16:45:35 -0700

    Update index.js

--- src/components/CommunityPage/index.js
+++ src/components/CommunityPage/index.js
@@ -99,7 +99,7 @@ const CommunityPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
 >
             <CodeBracketSquareIcon className={`h-6 w-6  ${textColor}`} />
           </a>
-	  <p>We're on GitHub :)</p>
+	  <p>Visit our GitHub!</p>
 	  <p></p>
 	  </div>
 
