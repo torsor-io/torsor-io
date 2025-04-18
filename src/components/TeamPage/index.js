3d3310081d4bb3c2b05a6d05172a61e418c9f344
David Wakeham <david@rsh.dev>
2025-04-18 14:13:11 -0700

    Update index.js

--- src/components/TeamPage/index.js
+++ src/components/TeamPage/index.js
@@ -102,7 +102,7 @@ const TeamPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
 >
 
 {/* <p className={`${subtleText}`}>Applying is simple! Please visit the <a href="/#puzzle" className={`hover:underline`}><tt>fbr</tt> terminal</a> and submit an application using the command <tt>dice > mail</tt>.</p>*/}
-<p className={`${subtleText}`}>We currently have no openings, but feel free to visit the <a href="/#puzzle" className={`hover:underline`}>terminal</a> and express interest via <tt>dice > mail</tt>.</p>
+<p className={`${subtleText}`}>We have no openings right now, but feel free to visit the <a href="/#puzzle" className={`hover:underline`}>terminal</a> and express interest via <tt>dice > mail</tt>.</p>
 </motion.div>
       
             {/* Existing Subscribe Section */}
