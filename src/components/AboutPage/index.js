282cfccf0cac004554d0f5fdbbbcf10dade7f893
David Wakeham <david@rsh.dev>
2025-07-17 22:51:04 -0700

    Update index.js

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -99,7 +99,7 @@ const AboutPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
     />
 </div>
             <b>Torsor Labs</b> performs fundamental research on quantum computing, AI, and distributed systems.
-	    We ensure our research is accessible and useful by offering consulting, open source software, and high-quality educational materials.
+	    We ensure our research is accessible and useful by offering consulting, open source software, and educational materials.
           </p>
         </motion.div>
 
