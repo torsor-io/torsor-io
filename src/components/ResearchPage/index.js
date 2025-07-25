e7c7906de0a3dd71a212ddb6405bdb2a13c1b755
ddub <david.a.wakeham@gmail.com>
2025-07-25 10:06:12 -0700

    Update index.js

--- src/components/ResearchPage/index.js
+++ src/components/ResearchPage/index.js
@@ -116,7 +116,7 @@ const ResearchPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
   </ul>
           </div>
 	                  {/* Paper Image */}
-          <div className="aspect-auto w-xs overflow-hidden">
+          <div className="aspect-video w-xs overflow-hidden">
                   <img
                     src="/assets/rocks/isbell.png"
                     alt="Isbell duality"
