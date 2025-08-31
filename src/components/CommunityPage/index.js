78a65ab5188747af40f3002369494c313d73706e
ddub <david.a.wakeham@gmail.com>
2025-08-31 11:00:48 -0700

    Update community index

--- src/components/CommunityPage/index.js
+++ src/components/CommunityPage/index.js
@@ -79,7 +79,8 @@ const CommunityPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
           • <a 
         href="/assets/yaw-docs/yaw101.html" 
         className={`hover:underline`}
-      ><b className={`font-comfortaa ${textColor} mb-3`}>yaw</b> 101</a>
+      ><b className={`font-comfortaa ${textColor} mb-3`}>yaw</b> 101</a> <br> <b>Difficulty</b>: ✪<br>
+<b>Prerequisites</br>: Nielsen and Chuang (chapters 1–2)
 	  </li>
   </ul>
                 </div>
