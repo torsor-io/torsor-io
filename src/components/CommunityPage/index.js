60c6ade0c64c3e33462b0d56226e76d423a8b1ba
ddub <david.a.wakeham@gmail.com>
2025-08-31 13:31:32 -0700

    Update index.js

--- src/components/CommunityPage/index.js
+++ src/components/CommunityPage/index.js
@@ -71,7 +71,7 @@ const CommunityPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
           Learning <b className={`font-comfortaa ${textColor}`}>yaw</b>
       </h2>
                   <p className={`${subtleText}`}>
-	  <b className={`font-comfortaa ${textColor} mb-3`}>yaw</b> is a domain-specific functional language for programming quantum computers (currently under development). If you want to learn to code quantum apps using high-school algebra, you've come to the right place!
+	  <b className={`font-comfortaa ${textColor} mb-3`}>yaw</b> is a domain-specific functional language for programming quantum computers (currently under development). If you want to learn to code quantum apps using high-school algebra, you're in the right place!
 	  </p>
 	  <div className={`border-t ${borderColor} mb-4 mt-10`}></div>
 <ul className="space-y-3">
