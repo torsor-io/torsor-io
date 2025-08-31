716e4f9a937dffb23dd5b2b0d13a362640fc73c4
ddub <david.a.wakeham@gmail.com>
2025-08-31 10:01:30 -0700

    Update index.js

--- src/components/CommunityPage/index.js
+++ src/components/CommunityPage/index.js
@@ -71,7 +71,7 @@ const CommunityPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
           Learning <a href="https://yaw.dev" className={`font-comfortaa ${textColor}`}>yaw</a>
       </h2>
                   <p className={`${subtleText}`}>
-	  <a href="https://yaw.dev" className={`font-comfortaa ${textColor} mb-3`}>yaw</a> is a domain-specific functional language for programming quantum computers (currently under development 🚧). If you want to learn to code using high-school algebra, you've come to the right place!
+	  <a href="https://yaw.dev" className={`font-comfortaa ${textColor} mb-3`}>yaw</a> is a domain-specific functional language for programming quantum computers (under development 🚧). If you want to learn to code using high-school algebra, you've come to the right place!
 	  </p>
 	  <div className={`border-t ${borderColor} mb-4 mt-10`}></div>
 <ul className="space-y-3">
