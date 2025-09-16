b57a6b543d27631bc0a52b4daf3a9375bce60724
David Wakeham <david@rsh.dev>
2025-09-15 22:47:47 -0700

    Update index.js

--- src/components/TeamPage/index.js
+++ src/components/TeamPage/index.js
@@ -102,7 +102,7 @@ const TeamPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
           Jon Male • Founder
                   </p>
                   <p className={`${subtleText} !mb-24`}>
-	  Jon is a seasoned technologist and entrepreneur with deep expertise in building innovative software platforms. As CTO of Axium for sixteen years, he architected Ajera, a comprehensive ERP system for architecture and engineering firms that was later acquired by Deltek. The founder of mindfulOS, he's pioneering ethical alternatives to conventional media platforms for young children. Jon brings his strong ethical foundation and decades of product development experience to the mission of shaping a Bell Labs for the quantum age.
+	  Jon is a seasoned technologist and entrepreneur with deep expertise in building innovative software platforms. As CTO of Axium for sixteen years, he architected Ajera, a comprehensive ERP system for architecture and engineering firms that was later acquired by Deltek. The founder of <a href="https://mindfulos.org/" className={`hover:underline`}>mindfulOS</a>, he's pioneering ethical alternatives to conventional media platforms for young children.
                   </p>
                   <div className="flex space-x-4">
 
