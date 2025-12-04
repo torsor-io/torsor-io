dd9d264b91af7123366d45b89e2dbed55e7c146e
David Wakeham <david@rsh.dev>
2025-12-03 23:00:51 -0800

    Update index.js

--- src/components/ConsultingPage/index.js
+++ src/components/ConsultingPage/index.js
@@ -99,7 +99,6 @@ const SelectedWorkPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
             transition={{ duration: 0.8 }}
             className="mb-12"
           >
-            <h1 className={`font-bold ${textColor} mb-4`}>Past Works</h1>
             <p className={`text-lg ${subtleText}`}>
 	  Here are a few projects from recent years (not under the Torsor moniker). We're open to exploring collaborations that aim to do something genuinely different, whether in research, software development or education.
             </p>
