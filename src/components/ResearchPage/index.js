f006034c5eea69e5e8844c5aacfe5dd55804ddeb
David Wakeham <david@rsh.dev>
2026-09-12 23:25:38 -0700

    Update index.js

--- src/components/ResearchPage/index.js
+++ src/components/ResearchPage/index.js
@@ -1,4 +1,4 @@
-import { AcademicCap } from '@heroicons/react/24/solid';
+import { AcademicCapIcon } from '@heroicons/react/24/solid';
 import WaveBackground from '../shared/WaveBackground';
 import { motion, AnimatePresence } from 'framer-motion';
 import Navbar from '../shared/Navbar';
@@ -165,7 +165,7 @@ const ResearchPage = ({ isDark, setIsDark, waveState, setWaveState }) => {
                 href="https://scholar.google.com/citations?user=WAgYEwYAAAAJ&hl=en" 
                 className={`inline-block ${buttonBg} ${textColor} text-l px-6 py-3 rounded-lg transition-colors border ${borderColor}`}
               >
-                <AcademicCap className={`h-6 w-6 ${textColor}`} />
+                <AcademicCapIcon className={`h-6 w-6 ${textColor}`} />
           </a>
           </div>
         </main>
